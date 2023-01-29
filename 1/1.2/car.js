9067c1c258ec0c3f3be8594429a5482e56ea56d6
MaximTarasov <51261927+MaximTarasoff@users.noreply.github.com>
2023-01-29 17:23:21 +0300

    Add files via upload

---
+++ 1/1.2/car.js
@@ -0,0 +1,17 @@
+const carName = 'Lada';
+let maxCarSpeed = 300;
+let carOwner = "Somebody";
+
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+
+maxCarSpeed = 200;
+console.log('У машины изменилась скорость', maxCarSpeed);
+maxCarSpeed = 150;
+console.log('У машины изменилась скорость', maxCarSpeed);
+maxCarSpeed = 90;
+console.log('У машины изменилась скорость', maxCarSpeed);
+
+carOwner = "Vitya";
+console.log('У машины изменился владелец', carOwner);
---
+++ 1/1.2/index.js
@@ -0,0 +1,17 @@
+// JavaScript-код с созданием переменных
+/* 
+Автор:
+Maxim Tarasov
+*/
+const myName = "Maxim";
+const mySurname = "Tarasov";
+let myFavoriteDrink = "Apple Juice";
+let myFavotiteAnimal = "Cat";
+const myFavoriteProgrammingLanguage = "C";
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavotiteAnimal);
+console.log(myFavoriteProgrammingLanguage);
+
