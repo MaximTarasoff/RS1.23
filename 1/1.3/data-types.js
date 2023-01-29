0f11ddadd6ee0f3dad6d01b50ac8dfcee55644a1
MaximTarasov <51261927+MaximTarasoff@users.noreply.github.com>
2023-01-29 19:32:45 +0300

    Add files via upload

---
+++ 1/1.3/data-types.js
@@ -0,0 +1,26 @@
+const age = 25;
+console.log('age', age, typeof age);
+
+const myName = "Maxim";
+console.log('myName', myName, typeof myName);
+
+const myStStatus = true;
+console.log('myStStatus', myStStatus, typeof myStStatus);
+
+const timeForWork = 41234123541234n;
+console.log('timeForWork', timeForWork, typeof timeForWork);
+
+const myNumber = console.log();
+console.log('myNumber', myNumber, typeof myNumber);
+
+const myInfo = {
+    name: myName,
+    age: age,
+};
+console.log('myInfo', myInfo, typeof myInfo);
+
+const idStd = Symbol("id543");
+console.log('idStd', idStd, typeof idStd);
+
+const mySecondName = null;
+console.log('mySecondName', mySecondName, typeof mySecondName);
